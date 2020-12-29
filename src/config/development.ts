400e615633613d48bb2f9302c67cca9291b8173e
fernancumez <fernancumez@gmail.com>
2020-12-28 20:18:45 -0600

    dev/prod environments initial setup (#1)

---
+++ src/config/development.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV !== "production") dotenv.config();
+
+const devConfig: IDevconfig = {
+  PORT: process.env.PORT || "4000",
+  NODE_ENV: process.env.NODE_ENV || "development",
+  CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:3000",
+  DATABASE_URL: process.env.DATABASE_URL || `school-db-${NODE_ENV}`,
+  USER: process.env.USER || "fernando",
+  PASSWORD: process.env.PASSWORD || "fernando",
+};
+
+interface IDevconfig {
+  PORT: string;
+  NODE_ENV: string;
+  CORS_ORIGIN: string;
+  DATABASE_URL: string;
+  USER: string;
+  PASSWORD: string;
+}
+
+export default devConfig;
