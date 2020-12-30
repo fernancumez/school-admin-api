763e165b9b57471bdc8b87d49ae1bb576c6cd1cc
fernancumez <fernancumez@gmail.com>
2020-12-29 22:03:29 -0600

    updated database_ur in dev/prod environments (#2)

--- src/config/development.ts
+++ src/config/development.ts
@@ -7,7 +7,9 @@ const devConfig: IDevconfig = {
   PORT: process.env.PORT || "4000",
   NODE_ENV: process.env.NODE_ENV || "development",
   CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:3000",
-  DATABASE_URL: process.env.DATABASE_URL || `school-db-${NODE_ENV}`,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.oxbda.mongodb.net/school-db-${NODE_ENV}?retryWrites=true&w=majority`,
   USER: process.env.USER || "fernando",
   PASSWORD: process.env.PASSWORD || "fernando",
 };
--- src/config/production.ts
+++ src/config/production.ts
@@ -4,7 +4,9 @@ const prodConfig: IProdconfig = {
   PORT: process.env.PORT || "6000",
   NODE_ENV: process.env.NODE_ENV || "production",
   CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:3000",
-  DATABASE_URL: process.env.DATABASE_URL || `school-db-${NODE_ENV}`,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    `mongodb+srv://fernando:fernando@cluster0.oxbda.mongodb.net/school-db-${NODE_ENV}?retryWrites=true&w=majority`,
   USER: process.env.USER || "fernando",
   PASSWORD: process.env.PASSWORD || "fernando",
 };
