ee44a23b39c15a3851f3a58472582e1fdb06e049
fernancumez <fernancumez@gmail.com>
2021-03-10 18:52:50 -0600

    added auth model config (#6)

---
+++ src/models/model.user.ts
@@ -0,0 +1,37 @@
+import bcrypt from "bcrypt";
+import { model, Schema } from "mongoose";
+import { IUser } from "../types/model.user.types";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre<IUser>("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+export default model<IUser>("User", userSchema);
